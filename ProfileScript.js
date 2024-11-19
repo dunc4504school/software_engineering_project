647fb18b7252abe007aebc96f0b24a1f786074f7
gheslip61 <gavinheslip00@gmail.com>
2024-11-18 21:22:33 -0500

    Delete ProfileScript.js

--- ProfileScript.js
+++
@@ -1,6 +0,0 @@
-// script.js
-function previewImage(event) {
-    const preview = document.getElementById('profile-preview');
-    preview.style.display = 'block';
-    preview.src = URL.createObjectURL(event.target.files[0]);
-}
