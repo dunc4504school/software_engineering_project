88ab9a2eee5b03eafcbebb6b8c7bf302103380dd
Lucas <dunc4504@mylaurier.ca>
2024-11-20 23:19:47 -0500

    Cleaning SQL_queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,18 +1,25 @@
+
+#Adds A Type: (Movie, Tv-Show)
 def add_type():
     return """INSERT INTO type (name) VALUES (%s)"""
 
+#Adds A Genre: (Documentary, Action, Mystery)
 def add_genre():
     return """INSERT INTO genre (name) VALUES (%s)"""
 
+#Adds Media
 def add_media():
     return """INSERT INTO media(type,
                                  genre,
+                                 genre2,
+                                 genre3,
                                  date_released,
                                  studio,
                                  producer,
                                  name) 
-               VALUES (%s, %s, %s, %s, %s, %s)"""
+               VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 
+#Adds Account
 def add_account():
     return """INSERT INTO account(name, 
                                   username,
@@ -23,6 +30,7 @@ def add_account():
               VALUES(%s, %s, CURRENT_DATE, %s, %s, %s)
               ON CONFLICT (username) DO NOTHING"""
 
+#Adds A Following: (account_id now follows follows_id)
 def add_following():
     return """WITH new_follow AS (
                 INSERT INTO following (account_id, follows_id, date_followed)
@@ -38,24 +46,8 @@ def add_following():
              SET total_followers = total_followers + 1
              WHERE id = (SELECT follows_id FROM new_follow);"""
 
-def delete_following():
-    return """WITH deleted_follow AS (
-    DELETE FROM following
-    WHERE account_id = %s AND follows_id = %s
-    RETURNING account_id, follows_id
-    ), update_following AS (
-    UPDATE account
-    SET total_following = total_following - 1
-    WHERE id = (SELECT account_id FROM deleted_follow)
-    RETURNING id
-    )
-    UPDATE account
-    SET total_followers = total_followers - 1
-    WHERE id = (SELECT follows_id FROM deleted_follow);  
-    """
 
-
-
+#Adds a Review:
 def add_review():
     return """WITH new_review AS (
                 INSERT INTO review (account_id, media_id, rating, description, date_reviewed)
@@ -75,19 +67,45 @@ def add_review():
             )
             WHERE id = (SELECT media_id FROM new_review);"""
 
+#----------------------------------------------------------------------------------------
 
+#Deletes A Following: (account_id not DOESNT follow follow_id)
+def delete_following():
+    return """WITH deleted_follow AS (
+    DELETE FROM following
+    WHERE account_id = %s AND follows_id = %s
+    RETURNING account_id, follows_id
+    ), update_following AS (
+    UPDATE account
+    SET total_following = total_following - 1
+    WHERE id = (SELECT account_id FROM deleted_follow)
+    RETURNING id
+    )
+    UPDATE account
+    SET total_followers = total_followers - 1
+    WHERE id = (SELECT follows_id FROM deleted_follow);  
+    """
+
+#-----------------------------------------------------------------------------------------------
+
+#Returns Type Id Given Name
 def get_type():
     return """ SELECT id from type where name = %s """
 
+#Returns Genre Id Given Name
 def get_genre():
     return """ SELECT id from genre where name = %s"""
 
+#Returns {limit} number random media
 def get_random_media(limit):
     return f""" SELECT id from media ORDER BY RANDOM() LIMIT {limit}"""
 
+#Returns Account Id Given Username
 def get_matching_account():
     return """SELECT id from account where username = %s"""
 
+
+#Returns List Of Accounts With Similar Usernames
 def get_searched_account():
     return f"""SELECT id, 
                     name, 
@@ -100,6 +118,7 @@ def get_searched_account():
             WHERE username LIKE %s     
             """
 
+#Returns List Of Media With Similar Names
 def get_searched_media():
     return """SELECT id,
                      name,
@@ -114,6 +133,7 @@ def get_searched_media():
               WHERE name LIKE %s
     """
 
+#Returns media given id
 def get_media():
     return """SELECT id,
                      name,
@@ -128,6 +148,8 @@ def get_media():
               WHERE id = %s
     """
 
+
+#Returns a summary of account (for anouther persons accout)
 def get_account_summary():
     return f"""SELECT id, 
                     name, 
@@ -139,7 +161,7 @@ def get_account_summary():
             FROM ACCOUNT
             WHERE id = %s     
             """
-
+#Returns a summary of account (for your account)
 def get_self_summary():
     return """SELECT id, 
                     name, 
@@ -154,6 +176,7 @@ def get_self_summary():
             WHERE id = %s     
             """
 
+#Returns Account Info, if username and password match some record
 def attempt_login():
     return """
             SELECT * from account 
@@ -161,6 +184,7 @@ def attempt_login():
             password = %s
             """
 
+#Returns the list of reviews made by an account
 def get_account_review():
     return """
         SELECT m.name,
@@ -178,6 +202,7 @@ def get_account_review():
         GROUP BY r.media_id, r.rating, r.description, m.name, m.full_average, g.name, t.name;
     """
 
+#Returns the list of account that follow this account
 def get_account_follows():
     return """
     SELECT a.id, a.name, a.total_reviews, a.total_following, a.total_followers 
@@ -185,6 +210,8 @@ def get_account_follows():
     JOIN account a on f.account_id = a.id
     WHERE f.follows_id = %s;"""
 
+
+#Returns the list of acccounts that this account is following
 def get_account_following():
     return  """
     SELECT a.id, a.name, a.total_reviews, a.total_following, a.total_followers 
@@ -193,6 +220,7 @@ def get_account_following():
     WHERE f.account_id = %s;"""
 
 
+#Returns The Most Recent Reviews Made By People This Account Follows
 def get_account_recent():
     return """
     SELECT t.name AS type_name,
@@ -215,6 +243,3 @@ def get_account_recent():
     ORDER BY r.date_reviewed DESC  
     LIMIT 20;
     """
-
-
-
