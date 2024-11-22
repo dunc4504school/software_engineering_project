d8979b4d1b98a9adb9f61951e29b4bd3693661d3
gheslip61 <gavinheslip00@gmail.com>
2024-11-22 00:44:42 -0500

    Added accounts

--- application_setup_multiple_genres.py
+++ application_setup_multiple_genres.py
@@ -55,3 +55,46 @@ for movie in movies2:
 # Commit changes to the database
 conn.commit()
 
+#Sample Real Account (Used For Presentation)
+
+count_media = 50
+max_reviews_per_account = 10
+
+#Adding Account Names
+account_names = ['Venkat Gunturi', 'Gavin Heslip', 'Hashim Jama', 
+                 'Zach Reid', 'Lucas Duncan', 'John Doe', 'Jane Smith',
+                 'Kylie Tan', 'Bertha Blue', 'Tiffany Stoik']
+
+cur.executemany(sq.add_account(), [(account,"TESTING" + account, "TESTING" + account, "TESTING", "TESTING" ) for account in account_names])
+conn.commit()
+
+#Sample media
+cur.execute(sq.get_random_media(count_media))
+media_ids = cur.fetchall()
+
+#Sample Account Ids
+account_ids = []
+for name in account_names:
+    cur.execute(sq.get_matching_account(), ("TESTING" + name,))
+    account_ids.append(cur.fetchall()[0][0])
+
+
+#Create Sample Followings
+for id in account_ids:
+    follow_count = random.randint(1, len(account_ids) - 1)
+    account_ids_to_follow = random.sample(account_ids, follow_count)
+    for follower_id in account_ids_to_follow:
+        if follower_id == id: continue
+        cur.execute(sq.add_following(), (id, follower_id))
+conn.commit()  
+
+#Create Sample Reviews:
+for id in media_ids:
+    review_count = random.randint(1, max_reviews_per_account)
+    
+    account_ids_to_review = random.sample(account_ids, review_count)
+
+    for account_id in account_ids_to_review:
+        rating = random.randint(1, 10)
+        cur.execute(sq.add_review(), (account_id, id, rating, 'TESTING',))
+conn.commit()
